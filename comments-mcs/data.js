10b8e02f3cb8f7d8ed9d13bebb737e17ac933308
Eduard <e.mordvitskiy@gmail.com>
2021-02-18 10:13:50 +0200

    #7 add some data

--- comments-mcs/data.js
+++ comments-mcs/data.js
@@ -9,4 +9,24 @@ module.exports.comments = [
     author: 'Kate Chopin',
     text: 'That\'s great',
   },
+  {
+    id: 1003,
+    author: 'John Doe',
+    text: 'Something i saw before',
+  },
+  {
+    id: 1004,
+    author: 'Kate Chopin',
+    text: 'Yes, looks very familiar',
+  },
+  {
+    id: 1005,
+    author: 'Robert Patison',
+    text: 'But i found something new in this post',
+  },
+  {
+    id: 1006,
+    author: 'Kate Chopin',
+    text: 'I don\'t like this post',
+  },
 ];
--- posts-mcs/data.js
+++ posts-mcs/data.js
@@ -1,14 +1,20 @@
 module.exports.posts = [
   {
-    title: 'Post 1st',
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    title: 'Lorem ipsum...',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nibh velit, vehicula et accumsan lobortis, posuere sed diam. Quisque interdum massa leo, a elementum nisl volutpat non. Vestibulum cursus ultricies vestibulum. Mauris iaculis tristique tortor in vulputate. Maecenas malesuada massa eu urna consectetur semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus rutrum finibus tortor, et cursus orci rutrum vitae. Etiam dignissim nec magna at volutpat. In leo mi, fermentum a erat in, finibus vulputate augue. Ut eget libero turpis. Duis pellentesque nunc ac nunc semper, consectetur dictum dui consectetur.',
     author: 'Kate Chopin',
-    comments: [1001],
+    comments: [1001,1002],
   },
   {
-    title: 'Post 2nd',
-    text: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    title: 'Mauris dignissim...',
+    text: 'Mauris dignissim diam non tellus faucibus, ut pretium sem auctor. Nullam consectetur mattis turpis, a sodales justo rhoncus vitae. Aenean ac erat vel metus maximus egestas non et ligula. Nunc imperdiet ipsum urna, hendrerit hendrerit massa finibus sed. Vestibulum semper arcu accumsan enim fermentum lacinia. Quisque mollis neque nec maximus malesuada. Sed vitae ante quam. Fusce ultrices orci non pharetra faucibus. In hac habitasse platea dictumst. Ut nec elit id urna tempor ultricies sit amet in felis. Integer ullamcorper imperdiet nisi, eget malesuada nisl molestie vel. Pellentesque et magna nec risus lacinia fringilla. Curabitur urna leo, maximus sed purus fermentum, lacinia rhoncus sem.',
     author: 'Paul Auster',
-    comments: [1002],
+    comments: [1003,1004,1006],
+  },
+  {
+    title: 'Duis porttitor...',
+    text: 'Duis porttitor varius ipsum, congue lobortis felis dictum vitae. Nunc molestie leo ut dui scelerisque, eget ultricies eros vulputate. Duis ac felis nec sapien ultricies malesuada eget tempor quam. Donec mauris dolor, tempor rutrum neque in, porta tempor tellus. Sed vitae volutpat nisl. Phasellus odio leo, tincidunt ut tempus et, tempor sit amet nunc. Aenean hendrerit nibh laoreet, porta tellus eget, pretium ligula. Morbi accumsan, massa euismod volutpat aliquam, nunc magna molestie enim, id imperdiet quam tortor in ligula. Suspendisse in ex ut dolor porta pellentesque vitae id ligula. Aliquam sed molestie neque. Quisque malesuada nisl mi, vitae lobortis turpis luctus vitae. Aliquam sit amet elementum diam. Phasellus at justo arcu.',
+    author: 'Paul Auster',
+    comments: [1005],
   },
 ];
